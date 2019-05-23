dd40cce9a7c6f137359cc08a536f9c8dac0248a7
Doronlnd84 <doronlnd84@gmail.com>
2019-05-23 20:14:11 +0300

    insert spring init 2

---
+++ src/test/java/dani/springframework/dlpetclinic/DlPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package dani.springframework.dlpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DlPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
