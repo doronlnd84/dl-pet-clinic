03161350b5087a38d352f2d99ffd4b76aa753c96
Doronlnd84 <doronlnd84@gmail.com>
2019-06-14 12:27:27 +0300

    Add PetTypeService

---
+++ pet-clinic-data/src/main/java/dani/springFramework/dlpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package dani.springFramework.dlpetclinic.services;
+
+import dani.springFramework.dlpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>  {
+}
