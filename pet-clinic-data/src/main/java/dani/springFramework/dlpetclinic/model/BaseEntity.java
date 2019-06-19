a52358278c4c10083733be2a0cb63b11f5326d29
Doronlnd84 <doronlnd84@gmail.com>
2019-06-19 08:38:32 +0300

    Set BaseEntity to JPA

--- pet-clinic-data/src/main/java/dani/springFramework/dlpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/dani/springFramework/dlpetclinic/model/BaseEntity.java
@@ -1,8 +1,18 @@
 package dani.springFramework.dlpetclinic.model;
 
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
 private Long id;
 
     public Long getId() {
